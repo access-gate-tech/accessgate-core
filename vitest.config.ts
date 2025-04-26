535a7893856701e3f9c8be3640ec456ab31db523
Arnun <saarnun@centralpattana.co.th>
2025-04-26 21:43:37 +0700

    Refactor Vitest configuration and clean up CI workflow
    - Modified vitest.config.ts to disable CSS processing, include test files, and adjust coverage reporters.
    - Removed unnecessary Codecov configuration from ci.yml to streamline the CI workflow.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,19 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "node",
+    css: false,
     globals: true,
+    environment: "node",
+    include: ["test/**/*.test.ts"],
     coverage: {
       provider: "v8",
-      reporter: ["text", "html", "json", "lcov"],
-      exclude: [
-        "node_modules/**",
-        "dist/**",
-        "**/*.d.ts",
-        "**/*.test.ts",
-        "**/*.config.ts",
-        "**/types.ts",
-      ],
+      reporter: ["text", "json", "html"],
     },
   },
 });
